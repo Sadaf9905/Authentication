55bf627c8fa2c723e9425ba70730578102d55c3a
Sadaf9905 <sadafnallamandu@gmail.com>
2020-03-11 11:00:02 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,37 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB6X2gcRetkBTuOCjxYHUYWmtGX7B6r5O0',
+  authDomain: 'fire-face-9713e.firebaseapp.com',
+  databaseURL: 'https://fire-face-9713e.firebaseio.com',
+  projectId: 'fire-face-9713e',
+  storageBucket: 'fire-face-9713e.appspot.com',
+  messagingSenderId: '939702638082',
+  appId: '1:939702638082:web:617d8e06fc00818f4bb91b',
+  measurementId: 'G-5640RHNE5P'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    GooglePlus
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( private fire: AngularFireAuth, public ref: ChangeDetectorRef, public googleplus: GooglePlus ) {}
 
+  loginWithGoogle() {
+    this.fire.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    .then( () => {
+      this.fire.auth.getRedirectResult().then( res => {
+        this.ref.detectChanges();
+        console.log(res);
+      });
+    });
+  }
+
+
+  loginwithfb() {
+    this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res => {
+      console.log(res);
+    });
+  }
+  login() {
+    this.googleplus.login({
+      webClientId: '939702638082-c2fegsr1sc3nhvs93mt7iu8e0jbj11gm.apps.googleusercontent.com',
+      offline: true
+    })
+    .then(res => {
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+      .then(suc => {
+        alert('Login successfully');
+      }).catch(err => console.error('Error: ', err));
+    });
+  }
 }
+
